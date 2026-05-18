a9079e35e388050f50de625332b4dd616eaf3e74
Antonio Gomez Hernandez <antoniomgh@gmail.com>
2026-05-18 23:17:03 +0200

    EntityIds update

--- gh-sp-simplesamlphp/var-simplesamlphp/config/authsources.php
+++ gh-sp-simplesamlphp/var-simplesamlphp/config/authsources.php
@@ -20,7 +20,7 @@ $config = array(
 
         // The entity ID of this SP.
         // Can be NULL/unset, in which case an entity ID is generated based on the metadata URL.
-        'entityID' => 'http://secaas-labs-organization/sp/simplesamlphp',
+        'entityID' => 'http://secaas-labs-poc-01.org/sp/simplesamlphp',
 
         // The entity ID of the IdP this should SP should contact.
         // Can be NULL/unset, in which case the user will be shown a list of available IdPs.
